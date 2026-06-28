7739b3f37bda82bee06c890ea8ccb8cb5e56d683
владислав-090 <pastukhov013@gmail.com>
2026-06-28 12:35:30 +0600

    Move database config to environment variables

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module users-api-memory
 
 go 1.22.5
 
-require github.com/lib/pq v1.12.3 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
--- internal/database/database.go
+++ internal/database/database.go
@@ -2,15 +2,30 @@ package database
 
 import (
 	"database/sql"
-    _ "github.com/lib/pq"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 
 
 
 func Connect() (*sql.DB, error) {
-	connStr := "host=localhost port=5432 user=postgres password=87015445399Cover dbname=users_api sslmode=disable"
+	err := godotenv.Load()
+		if err != nil {
+			return nil, err
+		}
 
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
